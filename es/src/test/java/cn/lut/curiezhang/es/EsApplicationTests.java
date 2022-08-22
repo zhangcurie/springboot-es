51fda6bdd5cbd79719fd404ee90b5106f078f3f8
Curie ZHANG <zhjl@lut.edu.cn>
2022-08-22 18:24:18 +0800

    init

---
+++ es/src/test/java/cn/lut/curiezhang/es/EsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lut.curiezhang.es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
